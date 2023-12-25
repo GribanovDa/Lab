6d6a0ce7664c6d391659815e6973be7724fa0cb8
GribanovDa <gribanov.daniil@inbox.ru>
2023-12-25 23:05:11 +0300

    Add Lr1 and Lr2 by GribanovDa

---
+++ Gribanov_D_D_22VMz_KR1/ЛР1/ЛР1/ЛР1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <unordered_set>
+#include <string>
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    std::string input;
+
+    // Ввод строки от пользователя
+    std::cout << "Введите строку: ";
+    std::getline(std::cin, input);
+
+    // Используем unordered_set для отслеживания уникальных символов
+    std::unordered_set<char> uniqueChars;
+
+    // Новая строка без повторений
+    std::string result;
+
+    // Обходим каждый символ в исходной строке
+    for (char ch : input) {
+        // Если символ еще не встречался, добавляем его в результат и в уникальный набор
+        if (uniqueChars.insert(ch).second) {
+            result += ch;
+        }
+    }
+
+    // Вывод результата
+    std::cout << "Строка без повторений: " << result << std::endl;
+
+    return 0;
+}
